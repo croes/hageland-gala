ef2ecdb68404017fa9f750199f90b50d9070ccc3
Glenn Croes <glenncroes@gmail.com>
2017-12-03 13:14:05 +0100

    Add account linking

--- src/components/FacebookLoginButton.tsx
+++ src/components/FacebookLoginButton.tsx
@@ -2,18 +2,45 @@ import * as React from 'react';
 import * as Button from 'react-bootstrap/lib/Button';
 import * as firebase from 'firebase';
 import { RouteComponentProps, withRouter } from 'react-router';
+import { PasswordPrompt } from './PasswordPrompt';
 
 // tslint:disable:no-console
 
 interface FacebookLoginButtonState {
+  existingEmailAddress: string;
   showPasswordPrompt: boolean;
   passwordPrompResult: string;
 }
 
 class FacebookLoginButtonComponent extends React.Component<RouteComponentProps<{}>, FacebookLoginButtonState> {
 
+  _onPasswordEntered: ((password: string) => void) | null = null;
+  _onHide: (() => void) | null = null;
+  
+  constructor(props: RouteComponentProps<{}>) {
+    super(props);
+    this.state = {
+      showPasswordPrompt: false,
+      passwordPrompResult: '',
+      existingEmailAddress: ''
+    };
+  }
+
   promptUserForPassword = () => {
-   return "test";
+    const passwordPromise = new Promise<string>((resolve, reject) => {
+      this._onPasswordEntered = resolve;
+      this._onHide = () => {
+        reject(new Error('Password prompt closed'));
+      };
+      this.setState({...this.state, showPasswordPrompt: true, passwordPrompResult: ''});
+    });
+    passwordPromise.then(this.resetPasswordPromptListeners).catch(this.resetPasswordPromptListeners);
+    return passwordPromise;
+  }
+
+  resetPasswordPromptListeners = () => {
+    this._onHide = null;
+    this._onPasswordEntered = null;
   }
 
   handleButtonClick = () => {
@@ -22,34 +49,44 @@ class FacebookLoginButtonComponent extends React.Component<RouteComponentProps<{
     provider.addScope('public_profile');
     const auth = firebase.auth();
     auth.signInWithPopup(provider).then((result) => {
-      console.log(result.user);
       this.props.history.push('/reservaties');
     }).catch((error) => {
       if (error.code === 'auth/account-exists-with-different-credential') {
         const pendingCred = error.credential;
         const email = error.email;
-        auth.fetchProvidersForEmail(email).then((providers) => {
-          if (providers[0] === 'password') {
-            const password = this.promptUserForPassword(); // TODO: implement promptUserForPassword.
-            auth.signInWithEmailAndPassword(email, password).then(function(user) {
-              // Step 4a.
-              return user.link(pendingCred);
-            }).then(function() {
-              // Facebook account successfully linked to the existing Firebase user.
-              this.props.history.push('/reservaties');
-            });
-          }
+        auth.fetchProvidersForEmail(email)
+          .then((providers) => {
+            if (providers[0] === 'password') {
+              this.setState({...this.state, existingEmailAddress: email});
+              return this.promptUserForPassword();
+            } else {
+              throw new Error('Don\'t know what to do with provider:' + providers[0]);
+            }
+          }).then((password) => {
+          return auth.signInWithEmailAndPassword(email, password);
+        }).then((user: firebase.User) => {
+          return user.linkWithCredential(pendingCred);
+        }).then(() => {
+          this.props.history.push('/reservaties');
+        }).catch(linkAccountError => {
+          console.log('Error occured while linking account', linkAccountError);
         });
-      } else {
-        console.error('Error occured while signing in: ', error);
       }
-    }) ;
+    });
   }
 
   render() {
+    const {showPasswordPrompt, existingEmailAddress} = this.state;
     return (
       <Button bsSize="large" bsStyle="primary" block={true} onClick={this.handleButtonClick}>
-        <i className="fa fa-facebook-official" /> Login via Facebook
+        <i className="fa fa-facebook-official"/> Login via Facebook
+        <PasswordPrompt
+          showPrompt={showPasswordPrompt}
+          message={`Er bestaat al een account met email-adres ${existingEmailAddress}.
+                    Geef het wachtwoord in voor deze account.`}
+          onPasswordEntered={this._onPasswordEntered}
+          onHide={this._onHide}
+        />
       </Button>
     );
   }
--- src/components/PasswordPrompt.tsx
+++ src/components/PasswordPrompt.tsx
@@ -5,11 +5,13 @@ import * as Form from 'react-bootstrap/lib/Form';
 import * as FormGroup from 'react-bootstrap/lib/FormGroup';
 import * as FormControl from 'react-bootstrap/lib/FormControl';
 import * as ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import * as Button from 'react-bootstrap/lib/Button';
 
 interface PasswordPromptProps {
   showPrompt: boolean;
   message: string;
-  onPasswordEntered: (password: string) => void;
+  onPasswordEntered: ((password: string) => void) | null;
+  onHide: (() => void) | null;
 }
 
 interface PasswordPromptState {
@@ -26,31 +28,41 @@ export class PasswordPrompt extends React.Component<PasswordPromptProps, Passwor
   }
 
   handlePasswordInputChange = (event: React.FormEvent<FormControl>) => {
-    this.setState({...this.state, passwordValue: event.currentTarget.value});
+    // tslint:disable-next-line:no-any
+    this.setState({...this.state, passwordValue: (event.currentTarget as any).value});
+  }
+
+  handleSubmit = (event: React.FormEvent<Form>) => {
+    event.preventDefault();
+    if (this.props.onPasswordEntered) {
+      this.props.onPasswordEntered(this.state.passwordValue);
+    }
   }
 
   render() {
-    const {showPrompt, email, message} = this.props;
+    const {showPrompt, message, onHide} = this.props;
+    const {passwordValue} = this.state;
     return (
-        <Modal show={showPrompt} onHide={() => {}}>
-          <Modal.Header>
-            Geef paswoord in
-          </Modal.Header>
+        <Modal show={showPrompt} onHide={onHide || (() => undefined)}>
           <Modal.Body>
-            {message}
-            <Form horizontal={true}>
+            <Col sm={12}>{message}</Col>
+            <Form horizontal={true} onSubmit={this.handleSubmit}>
               <FormGroup controlId="loginPassword">
-                <Col componentClass={ControlLabel} sm={2}>
+                <Col componentClass={ControlLabel} sm={3}>
                   Wachtwoord
                 </Col>
-                <Col sm={10}>
+                <Col sm={9}>
                   <FormControl
                       type="password"
+                      value={passwordValue}
                       placeholder="Wachtwoord"
                       onChange={this.handlePasswordInputChange}
                   />
                 </Col>
               </FormGroup>
+              <Button bsStyle="primary" type="submit">
+                Bevestig
+              </Button>
             </Form>
           </Modal.Body>
         </Modal>
