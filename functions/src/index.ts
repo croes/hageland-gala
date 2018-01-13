bf866d061c1f0ba235b43fae5f75f861ea6ae7e0
Glenn Croes <glenncroes@gmail.com>
2018-01-13 14:37:11 +0100

    Add email trigger for diner reservation

---
+++ functions/src/index.ts
@@ -0,0 +1,51 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as nodemailer from "nodemailer";
+import { DinerReservation, translateMenuChoice } from '../../src/model';
+import { BANK_ACCOUNT, DINER_PRICE, DINER_STUDENT_PRICE, END_OF_RESERVATION_DATE_STRING } from '../../src/constants';
+admin.initializeApp(functions.config().firebase);
+
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword
+  }
+});
+
+// Sends an email confirmation when a user changes his mailing list subscription.
+exports.sendEmailConfirmationForDiner = functions.database.ref('/reservations/diner').onCreate(event => {
+  const snapshot = event.data;
+  const diner = snapshot.val() as DinerReservation;
+
+  const userUid = diner.createdBy;
+  const userPromise = admin.auth().getUser(userUid);
+
+  return userPromise.then(user => {
+    const mailOptions = {
+      from: '"Hageland Galabalcomité" <noreply@hagelandgala.be>',
+      to: user.email,
+      subject: "Hageland Galabal - reservatie diner",
+      text: `Hallo ${user.displayName},\n` +
+            `\n` +
+            `We hebben de volgende reservatie voor het Hageland diner genoteerd:\n` +
+            `\n` +
+            `Naam: ${diner.reserveeName}\n` +
+            `Menu: ${translateMenuChoice(diner.menuChoice)}\n` +
+            `\n` +
+            `Gelieve ${DINER_PRICE} euro (${DINER_STUDENT_PRICE} euro voor actieve studenten) over te schrijven op ${BANK_ACCOUNT} voor ${END_OF_RESERVATION_DATE_STRING}.\n` +
+            `Je reservatie is pas definitief als we de betaling ontvangen hebben!\n` +
+            `\n` +
+            `Tot op het galabal!\n` +
+            `\n` +
+            `Met vriendelijke groeten\n` +
+            ` - Het Hageland Galabalcomité\n`
+    };
+
+    return mailTransport.sendMail(mailOptions)
+      .then(() => console.log(`Diner confirmation email sent to:`, user.email))
+      .catch(error => console.error(`There was an error while sending a Diner confirmation email to ${user.email}`, error));
+  });
+});
---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const DINER_PRICE = 80;
+export const DINER_STUDENT_PRICE = 75;
+export const PARTY_PRICE = 0;
+export const BANK_ACCOUNT = 'BE92 3770 1835 1023';
+export const END_OF_RESERVATION_DATE_STRING = '10 maart 2018';
