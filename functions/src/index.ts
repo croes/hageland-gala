13cdfa8f13d68429ce3160c4d9c584a2c5bcfb87
Glenn Croes <glenncroes@gmail.com>
2018-01-14 20:39:51 +0100

    More email triggers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,9 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import * as nodemailer from "nodemailer";
-import { DinerReservation, translateMenuChoice } from './model';
+import * as nodemailer from 'nodemailer';
+import { DinerReservation, translateMenuChoice, ReservationStatus } from './model';
 import { BANK_ACCOUNT, DINER_PRICE, DINER_STUDENT_PRICE, END_OF_RESERVATION_DATE_STRING } from './constants';
+import { BusReservation } from '../../src/model';
 admin.initializeApp(functions.config().firebase);
 
 const gmailEmail = functions.config().gmail.email;
@@ -20,7 +21,7 @@ export const sendEmailConfirmationForDiner = functions.database.ref('/reservatio
   const snapshot = event.data;
   const diner = snapshot.val() as DinerReservation;
 
-  console.log("New diner reservation (key: [" + event.params.dinerKey + "]) found. Sending confirmation email.");
+  console.log('New diner reservation (key: [' + event.params.dinerKey + ']) found. Sending confirmation email.');
 
   const userUid = diner.createdBy;
   const userPromise = admin.auth().getUser(userUid);
@@ -29,10 +30,10 @@ export const sendEmailConfirmationForDiner = functions.database.ref('/reservatio
     const mailOptions = {
       from: '"Hageland Galabalcomité" <hagelandgalabal@gmail.com>',
       to: user.email,
-      subject: "Hageland Galabal - reservatie diner",
+      subject: 'Hageland Galabal - reservatie diner',
       text: `Hallo ${user.displayName},\n` +
             `\n` +
-            `We hebben de volgende reservatie voor het Hageland diner genoteerd:\n` +
+            `We hebben de volgende reservatie voor het galabal diner genoteerd:\n` +
             `\n` +
             `Naam: ${diner.reserveeName}\n` +
             `Menu: ${translateMenuChoice(diner.menuChoice)}\n` +
@@ -48,6 +49,151 @@ export const sendEmailConfirmationForDiner = functions.database.ref('/reservatio
 
     return mailTransport.sendMail(mailOptions)
       .then(() => console.log(`Diner confirmation email sent to:`, user.email))
-      .catch(error => console.error(`There was an error while sending a Diner confirmation email to ${user.email}`, error));
+      .catch(error => console.error(`There was an error while sending a diner confirmation email to ${user.email}`, error));
+  });
+});
+
+export const sendEmailConfirmationForDinerCancellation = functions.database.ref('/reservations/diner/{dinerKey}').onDelete(event => {
+  const snapshot = event.data;
+  const diner = snapshot.val() as DinerReservation;
+
+  console.log('Diner reservation removed (key: [' + event.params.dinerKey + ']) found. Sending cancellation email.');
+
+  const userUid = diner.createdBy;
+  const userPromise = admin.auth().getUser(userUid);
+
+  return userPromise.then(user => {
+    const mailOptions = {
+      from: '"Hageland Galabalcomité" <hagelandgalabal@gmail.com>',
+      to: user.email,
+      subject: 'Hageland Galabal - annulatie reservatie diner',
+      text: `Hallo ${user.displayName},\n` +
+      `\n` +
+      `We hebben de volgende reservatie voor het galabal diner geannuleerd:\n` +
+      `\n` +
+      `Naam: ${diner.reserveeName}\n` +
+      `Menu: ${translateMenuChoice(diner.menuChoice)}\n` +
+      `\n` +
+      `Met vriendelijke groeten\n` +
+      ` - Het Hageland Galabalcomité\n`
+    };
+
+    return mailTransport.sendMail(mailOptions)
+      .then(() => console.log(`Diner cancellation email sent to:`, user.email))
+      .catch(error => console.error(`There was an error while sending a Diner cancellation email to ${user.email}`, error));
+  });
+});
+
+export const sendEmailConfirmationForBus = functions.database.ref('/reservations/bus/{busKey}').onCreate(event => {
+  const snapshot = event.data;
+  const busReservation = snapshot.val() as BusReservation;
+
+  console.log('New bus reservation (key: [' + event.params.busKey + ']) found. Sending confirmation email.');
+
+  const userUid = busReservation.createdBy;
+  const userPromise = admin.auth().getUser(userUid);
+
+  return userPromise.then(user => {
+    const mailOptions = {
+      from: '"Hageland Galabalcomité" <hagelandgalabal@gmail.com>',
+      to: user.email,
+      subject: 'Hageland Galabal - reservatie bus',
+      text: `Hallo ${user.displayName},\n` +
+      `\n` +
+      `We hebben de volgende reservatie voor de bus van/naar het galabal genoteerd:\n` +
+      `\n` +
+      `Naam: ${busReservation.reserveeName}\n` +
+      `Busritten:\n` +
+      (busReservation.dinerBus ? `Bus voor het diner: Gerechtsgebouw Leuven (17:00) -> Lindenhof Baal (18:00)\n` : '') +
+      (busReservation.partyBus ? `Bus voor het avondfeest: Gerechtsgebouw Leuven (21:00) -> Lindenhof Baal, (22:00)\n` : '') +
+      (busReservation.nightBus ? `Bus terug naar Leuven: Lindenhof Baal (03:45) -> Gerechtsgebouw Leuven (04:30))\n` : '') +
+      `\n` +
+      `Tot op het galabal!\n` +
+      `\n` +
+      `Met vriendelijke groeten\n` +
+      ` - Het Hageland Galabalcomité\n`
+    };
+
+    return mailTransport.sendMail(mailOptions)
+      .then(() => console.log(`Bus confirmation email sent to:`, user.email))
+      .catch(error => console.error(`There was an error while sending a bus confirmation email to ${user.email}`, error));
+  });
+});
+
+const sendEmailConfirmationForBusCancellation = functions.database.ref('/reservations/bus/{busKey}').onDelete(event => {
+  const snapshot = event.data;
+  const busReservation = snapshot.val() as BusReservation;
+
+  console.log('Deleted bus reservation (key: [' + event.params.busKey + ']) found. Sending cancellation email.');
+
+  const userUid = busReservation.createdBy;
+  const userPromise = admin.auth().getUser(userUid);
+
+  return userPromise.then(user => {
+    const mailOptions = {
+      from: '"Hageland Galabalcomité" <hagelandgalabal@gmail.com>',
+      to: user.email,
+      subject: 'Hageland Galabal - annulatie reservatie bus',
+      text: `Hallo ${user.displayName},\n` +
+      `\n` +
+      `We hebben de volgende reservatie voor de bus van/naar het galabal geannuleerd:\n` +
+      `\n` +
+      `Naam: ${busReservation.reserveeName}\n` +
+      `Busritten:\n` +
+      (busReservation.dinerBus ? `Bus voor het diner: Gerechtsgebouw Leuven (17:00) -> Lindenhof Baal (18:00)\n` : '') +
+      (busReservation.partyBus ? `Bus voor het avondfeest: Gerechtsgebouw Leuven (21:00) -> Lindenhof Baal, (22:00)\n` : '') +
+      (busReservation.nightBus ? `Bus terug naar Leuven: Lindenhof Baal (03:45) -> Gerechtsgebouw Leuven (04:30))\n` : '') +
+      `\n` +
+      `Tot op het galabal!\n` +
+      `\n` +
+      `Met vriendelijke groeten\n` +
+      ` - Het Hageland Galabalcomité\n`
+    };
+
+    return mailTransport.sendMail(mailOptions)
+      .then(() => console.log(`Bus cancellation email sent to:`, user.email))
+      .catch(error => console.error(`There was an error while sending a bus cancellation email to ${user.email}`, error));
+  });
+});
+
+const sendEmailConfirmationForPaymentReceived = functions.database.ref('/rervations/diner/{dinerKey}').onUpdate(event => {
+  const snapshot = event.data;
+  const dinerBefore = snapshot.previous.val() as DinerReservation;
+  const dinerAfter = snapshot.current.val() as DinerReservation;
+
+  const dinerJustGotPaid = (dinerBefore.status !== ReservationStatus.PAID && dinerAfter.status === ReservationStatus.PAID);
+
+  if (!dinerJustGotPaid) {
+    return Promise.resolve();
+  }
+
+  console.log('Diner was paid (key: [' + event.params.dinerKey + ']) found. Sending confirmation email.');
+
+  const userUid = dinerAfter.createdBy;
+  const userPromise = admin.auth().getUser(userUid);
+
+  return userPromise.then(user => {
+    const mailOptions = {
+      from: '"Hageland Galabalcomité" <hagelandgalabal@gmail.com>',
+      to: user.email,
+      subject: 'Hageland Galabal - bevestiging ontvangst betaling',
+      text: `Hallo ${user.displayName},\n` +
+      `\n` +
+      `We hebben de betaling voor het onderstaande galabal diner goed ontvangen:\n` +
+      `\n` +
+      `Naam: ${dinerAfter.reserveeName}\n` +
+      `Menu: ${translateMenuChoice(dinerAfter.menuChoice)}\n` +
+      `\n` +
+      `Je reservatie is nu definitief!\n` +
+      `\n` +
+      `Tot op het galabal!\n` +
+      `\n` +
+      `Met vriendelijke groeten\n` +
+      ` - Het Hageland Galabalcomité\n`
+    };
+
+    return mailTransport.sendMail(mailOptions)
+      .then(() => console.log(`Diner payment confirmation email sent to:`, user.email))
+      .catch(error => console.error(`There was an error while sending a diner payment confirmation email to ${user.email}`, error));
   });
 });
